001767f0b0405283ce56ae536ad683d83005bed3
GiancarloFusiello <g.fusiello@elifesciences.org>
2019-05-31 14:56:37 +0100

    split task_helpers.py tests into single tests to remove test conditional logic

--- tests/test_task_helpers.py
+++ tests/test_task_helpers.py
@@ -1,5 +1,3 @@
-import pytest
-
 from dags.task_helpers import (
     get_previous_task_name,
     get_return_value_from_previous_task
@@ -7,27 +5,25 @@ from dags.task_helpers import (
 from tests.factories import TaskInstanceFactory
 
 
-@pytest.mark.parametrize('has_prev_task, expected', [
-    (True, 'previous_task'),
-    (False, None)
-])
-def test_get_previous_task_name(has_prev_task, expected, context):
-    if not has_prev_task:
-        context = TaskInstanceFactory().get_template_context()
-
+def test_get_previous_task_name(context):
     result = get_previous_task_name(**context)
-    assert result == expected
+    assert result == 'previous_task'
 
 
-@pytest.mark.parametrize('has_prev_task, expected', [
-    (True, 'previous_return_value'),
-    (False, None)
-])
-def test_get_return_value_from_previous_task(has_prev_task, expected, context):
-    if not has_prev_task:
-        context = TaskInstanceFactory().get_template_context()
-    else:
-        context['task_instance'].xcom_pull = lambda **kwargs: expected
+def test_get_previous_task_name_without_previous_task():
+    context = TaskInstanceFactory().get_template_context()
+    result = get_previous_task_name(**context)
+    assert result is None
 
+
+def test_get_return_value_from_previous_task(context):
+    expected = 'previous_return_value'
+    context['task_instance'].xcom_pull = lambda **kwargs: expected
     result = get_return_value_from_previous_task(**context)
     assert result == expected
+
+
+def test_get_return_value_from_previous_task_without_return_value():
+    context = TaskInstanceFactory().get_template_context()
+    result = get_return_value_from_previous_task(**context)
+    assert result is None
