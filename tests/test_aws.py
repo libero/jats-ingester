3c9b8b991cb3577cca5d71d34afb044225c12062
GiancarloFusiello <g.fusiello@elifesciences.org>
2019-05-31 14:25:36 +0100

    remove mock responses and conditional logic for test_list_bucket_keys_iter and use test parameters

--- tests/test_aws.py
+++ tests/test_aws.py
@@ -2,7 +2,6 @@ import pytest
 
 from dags.aws import get_aws_connection, list_bucket_keys_iter
 from dags.trigger_dag import SOURCE_BUCKET, DESTINATION_BUCKET
-from tests import mocks
 
 
 @pytest.mark.parametrize('url, expected', [
@@ -18,29 +17,37 @@ def test_get_aws_connection(url, expected):
 
 
 @pytest.mark.parametrize('params, response, expected', [
-    ({'Bucket': SOURCE_BUCKET, 'Delimiter': '.zip'},
-     mocks.list_objects_v2_source_bucket_delimiter,
-     ['elife-666-vor-r1.zip']),
-    ({'Bucket': SOURCE_BUCKET, 'Delimiter': '.zip'},
-     mocks.list_objects_v2_source_bucket_delimiter,
-     []),
-    ({'Bucket': DESTINATION_BUCKET, 'Delimiter': '/'},
-     mocks.list_objects_v2_destination_bucket_delimiter,
-     ['elife-666-vor-r1/']),
-    ({'Bucket': DESTINATION_BUCKET, 'Delimiter': '/'},
-     mocks.list_objects_v2_destination_bucket_delimiter,
-     []),
-    ({'Bucket': SOURCE_BUCKET},
-     mocks.list_objects_v2_source_bucket,
-     ['elife-666-vor-r1.zip']),
-    ({'Bucket': SOURCE_BUCKET},
-     mocks.list_objects_v2_source_bucket,
-     [])
+    (
+        {'Bucket': SOURCE_BUCKET, 'Delimiter': '.zip'},
+        {'CommonPrefixes':[{'Prefix': 'elife-666-vor-r1.zip'}]},
+        ['elife-666-vor-r1.zip']
+    ),
+    (
+        {'Bucket': SOURCE_BUCKET, 'Delimiter': '.zip'},
+        {},
+        []
+    ),
+    (
+        {'Bucket': DESTINATION_BUCKET, 'Delimiter': '/'},
+        {'CommonPrefixes':[{'Prefix': 'elife-666-vor-r1/'}]},
+        ['elife-666-vor-r1/']
+    ),
+    (
+        {'Bucket': DESTINATION_BUCKET, 'Delimiter': '/'},
+        {},
+        []
+    ),
+    (
+        {'Bucket': SOURCE_BUCKET},
+        {'Contents':[{'Key': 'elife-666-vor-r1.zip'}]},
+        ['elife-666-vor-r1.zip']
+    ),
+    (
+        {'Bucket': SOURCE_BUCKET},
+        {},
+        []
+    )
 ])
-def test_list_bucket_keys_iter(params, response, expected, mocker, s3_client):
-    if not expected:
-        response.pop('Contents', None)
-        response.pop('CommonPrefixes', None)
-
+def test_list_bucket_keys_iter(params, response, expected, s3_client):
     keys = list_bucket_keys_iter(response=[response], **params)
     assert list(keys) == expected
