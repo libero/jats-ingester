1a3e62d5dfb6f9243f538cc454afedbc4a4471c4
GiancarloFusiello <g.fusiello@elifesciences.org>
2019-06-25 10:03:15 +0100

    add docstring to mock s3 client _read_bytes method

--- tests/mocks.py
+++ tests/mocks.py
@@ -16,6 +16,13 @@ class s3ClientMock:
         return self
 
     def _read_bytes(self, file_name):
+        """
+        Trys to find the file in the project and read it. If the file is not
+        located, try to locate the file in a zip file if `file_name` is a path.
+
+        :param str file_name: name of file or file path
+        :return bytes: bytes of read file
+        """
         try:
             return get_asset(file_name).read_bytes()
         except FileNotFoundError:
