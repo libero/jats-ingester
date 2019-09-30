738f5c8b33f7a5d1f80af86517964bcd6ce491e1
GiancarloFusiello <g.fusiello@elifesciences.org>
2019-09-30 09:47:03 +0100

    get categories text from within child elements.

--- dags/libero/xml/__init__.py
+++ dags/libero/xml/__init__.py
@@ -21,10 +21,14 @@ def get_element_text_from_xpaths(xml: ElementTree, xpaths: Iterable[str], namesp
     Searches an lxml ElementTree object for all elements xpaths
     and returns a list of text from each element found.
     """
-    elements = []
+    elements_text = []
     for xpath in xpaths:
         results = xml.xpath(xpath, namespaces=namespaces)
-        elements.extend([e.text.strip() for e in results if e.text is not None])
+        # get text from element or from all child elements
+        results = [e.text.strip()
+                   if e.text is not None else e.xpath('string()').strip()
+                   for e in results]
+        elements_text.extend([r for r in results if r])
 
-    assert elements, 'XPaths not found in xml: %s' % str(xpaths)
-    return elements
+    assert elements_text, 'XPaths not found in xml: %s' % str(xpaths)
+    return elements_text
