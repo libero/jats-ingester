9d922fb952582ed7cf42ea6b83741206b0f79b65
GiancarloFusiello <g.fusiello@elifesciences.org>
2019-10-09 15:17:53 +0100

    remove debug statements

--- .travis/smoke-test.sh
+++ .travis/smoke-test.sh
@@ -9,12 +9,8 @@ function finish {
 trap finish EXIT
 
 docker-compose up -d
-echo "docker ps"
-docker ps
-echo "sleep 2"
-sleep 2
-echo "docker ps"
-docker ps
-name=$(docker ps | grep airflow_webserver | awk '{print $1}')
+
+name=$(docker ps | awk '/airflow_webserver/ {print $NF}')
+echo "Waiting for service " + $name +  " to be healthy"
 .scripts/docker/wait-healthy.sh $name 60
 ./scripts/airflow-status-check.sh localhost:8080
