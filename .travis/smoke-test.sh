33b4fc5de9b0e536413486e2963c6372a7f7ff35
GiancarloFusiello <g.fusiello@elifesciences.org>
2019-10-09 14:53:23 +0100

    print running containers

--- .travis/smoke-test.sh
+++ .travis/smoke-test.sh
@@ -9,7 +9,12 @@ function finish {
 trap finish EXIT
 
 docker-compose up -d
-
+echo "docker ps"
+docker ps
+echo "sleep 2"
+sleep 2
+echo "docker ps"
+docker ps
 name=$(docker ps | grep airflow_webserver | awk '{print $1}')
 .scripts/docker/wait-healthy.sh $name 60
 ./scripts/airflow-status-check.sh localhost:8080
