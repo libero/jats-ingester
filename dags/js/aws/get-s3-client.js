391186fc2f2bc18a0d34fcc5c2d8f5094ea4936d
GiancarloFusiello <g.fusiello@elifesciences.org>
2019-10-21 16:45:20 +0100

    set s3ForcePathStyle for local usage only

--- dags/js/aws/get-s3-client.js
+++ dags/js/aws/get-s3-client.js
@@ -5,14 +5,15 @@ function getS3Client() {
   let s3ConfigParams = {
     apiVersion: '2006-03-01',
     maxRetries: 3,
-    s3ForcePathStyle: true,
     httpOptions: {
       connectTimeout: 5000
     }
   };
+
   // set s3 to use local container by setting the ENDPOINT_URL environment variable
   if (process.env.ENDPOINT_URL) {
     s3ConfigParams.endpoint = new AWS.Endpoint(process.env.ENDPOINT_URL);
+    s3ConfigParams.s3ForcePathStyle = true;
   }
 
   console.log('AWS S3 params: ', s3ConfigParams);
